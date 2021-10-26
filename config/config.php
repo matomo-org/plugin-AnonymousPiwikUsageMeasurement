980a18b5abe94f2ba1248227113b0cbe5c93a1b8
Altamash Shaikh <altu9594@gmail.com>
2021-10-26 09:28:51 +0530

    Added code to update csp polic header based on custom site url, #59

--- config/config.php
+++ config/config.php
@@ -2,5 +2,15 @@
 
 return array(
     'AnonymousPiwikUsageMeasurement.piwikVersion' => \Piwik\Version::VERSION,
-    'AnonymousPiwikUsageMeasurement.phpVersion' => phpversion()
+    'AnonymousPiwikUsageMeasurement.phpVersion' => phpversion(),
+    \Piwik\View\SecurityPolicy::class => DI\decorate(function ($previous) {
+        /** @var \Piwik\View\SecurityPolicy $previous */
+
+        $settings = new Piwik\Plugins\AnonymousPiwikUsageMeasurement\SystemSettings();
+        $customSiteUrl = $settings->getSetting('customSiteUrl')->getValue();
+        if (!empty($customSiteUrl)) {
+            $previous->addPolicy('default-src', parse_url($customSiteUrl, PHP_URL_HOST));
+        }
+        return $previous;
+    })
 );
