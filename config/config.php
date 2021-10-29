2a7bc14240a8c0b90e8b5815fbb14c40364c2b66
sgiehl <stefan@matomo.org>
2021-10-29 16:09:57 +0200

    Fix plugin might break the Matomo installer if shipped together

--- config/config.php
+++ config/config.php
@@ -6,6 +6,11 @@ return array(
     'Piwik\View\SecurityPolicy' => DI\decorate(function ($previous) {
         /** @var \Piwik\View\SecurityPolicy $previous */
 
+        if (!\Piwik\SettingsPiwik::isMatomoInstalled()) {
+            // if Matomo is not yet installed there can't be any system setting
+            return $previous;
+        }
+
         $settings = new Piwik\Plugins\AnonymousPiwikUsageMeasurement\SystemSettings();
         $customSiteUrl = $settings->getSetting('customSiteUrl')->getValue();
         $host = parse_url($customSiteUrl, PHP_URL_HOST);
